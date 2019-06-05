015ff85cde73d32e5f0c9063cbe1f8fe201a9050
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-05 18:45:30 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,8 +3,7 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     outputArr = []
-    input = Hash
-    input.each do |key, value|
+    self.each do |key, value|
       arguments.each do |argument|
         if argument == value
           outputArr << key
